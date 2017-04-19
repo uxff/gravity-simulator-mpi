3dce7905855655b4c905292d980f97afc152c5e8
xuduorui <xuduorui@xqshijie.cn>
2017-04-19 21:46:45 +0800

    全部double化

--- main_orbs.c
+++ main_orbs.c
@@ -9,24 +9,24 @@
 #define G 0.0000005
 #define MIN_DIST 1.0
 const int unitSize = 8;//unitSize:每个计算单位的大小，多少个float
-float wide = 1000;
-float mass = 10;
-float velo = 0.005;
+double wide = 1000;
+double mass = 10;
+double velo = 0.005;
 
-typedef struct orb {
-  float x,y,z,vx,vy,vz,m,st;
-}orb;
+typedef struct Orb {
+  double x,y,z,vx,vy,vz,m,st;
+}Orb;
 typedef union {
-  orb orbst;
-  float fmem[8];
-}uorb;
+  Orb orbst;
+  double fmem[8];
+}UOrb;
 
-void initList(orb *olist, int nUnit, int style);
-void calcOne(orb*o, int oId, orb*olist, int nUnit);
-void calcGravity(orb*o, orb*ta, float dist, float*gx, float*gy, float*gz);
-void printList(float *list, int len, int rank, const char* s);
-void saveList(orb *olist, int len, const char* filepath);
-void loadList(float **list, int *len, const char* filepath);
+void initList(Orb *olist, int nUnit, int style);
+void calcOne(Orb*o, int oId, Orb*olist, int nUnit);
+void calcGravity(Orb*o, Orb*ta, double dist, double*gx, double*gy, double*gz);
+void printList(double *list, int len, int rank, const char* s);
+void saveList(Orb *olist, int len, const char* filepath);
+void loadList(double **list, int *len, const char* filepath);
 
 int main(int argc, char *argv[])
 {
@@ -62,52 +62,53 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nWorkers);
-    double startTime = MPI_Wtime(), endTime;
     // 任务如何均匀下发？
-    int nUnitPerWorker = nUnit / nWorkers;//nUnit:list有多少个unit。list字节长度=sizeof(float)*unitSize
-    int listSizeOfByte = sizeof(float) * unitSize * nUnit;
+    int nUnitPerWorker = nUnit / nWorkers;//nUnit:list有多少个unit。list字节长度=sizeof(double)*unitSize
+    int listSizeOfByte = sizeof(double) * unitSize * nUnit;
     int chunkSize = unitSize*nUnitPerWorker;
-    float* list = (float*)malloc(listSizeOfByte);
+    double* list = (double*)malloc(listSizeOfByte);
     long calcTimes = (long)(nUnit * nUnit) * (long)nTimes;
-    printf("list addr=%p\n", list);
+    printf("list addr=%p nUnit=%d nUnitPerWorker=%d chunkSize=%d listbytes=%d\n", list, nUnit, nUnitPerWorker, chunkSize, listSizeOfByte);
 
     // init list here
     if (rank == 0) {
-    initList((orb*)list, nUnit, 0);
-    printList(list, nUnit*unitSize, rank, "after init bcast");
-        MPI_Bcast(list, nUnit*unitSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        initList((Orb*)list, nUnit, 0);
     }
-    
+    // 广播必须每个rank都调用，否则报 Fatal error in PMPI_Barrier: Message truncated, error stack
+    MPI_Bcast(list, nUnit*unitSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    printList(list, nUnit*unitSize, rank, "after init bcast");
+    double startTime = MPI_Wtime(), endTime;
 
     int i = 0, j = 0, k = 0;
     for (j=0; j<nTimes; ++j) {
         // calculate list here
-        //list[rank*chunkSize] = (float)rank*100+(float)j;
+        //list[rank*chunkSize] = (double)rank*100+(double)j;
         for (k=0; k<nUnitPerWorker; ++k) {
-            orb* o = (orb*)(list+rank*nUnitPerWorker*unitSize+k);
+            Orb* o = (Orb*)(list+rank*nUnitPerWorker*unitSize+k);
             int oId = rank*nUnitPerWorker+k;
-            calcOne(o, oId, (orb*)list, nUnit);
+            calcOne(o, oId, (Orb*)list, nUnit);
         }
-        //sleep(1);
 
         MPI_Barrier(MPI_COMM_WORLD);
         // bcast 对于发送方来说等于send，对于接受方来说等于recv，调用时需要for i in nWorkers来依次bcast。相当于调用nWorkers*nWorkers次。
         // 不加for循环，root设置为自己，相当于只有root.send,没有other.recv。
-        for (i=0; i<nWorkers; ++i) MPI_Bcast(list+i*chunkSize, chunkSize, MPI_FLOAT, i, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
-        //printList(list, nUnit*unitSize, rank, "after 2nd bcast");
+        for (i=0; i<nWorkers; ++i) MPI_Bcast(list+i*chunkSize, chunkSize, MPI_DOUBLE, i, MPI_COMM_WORLD);
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+    printList(list, nUnit*unitSize, rank, "finally");
+    printf("list+i*chunkSize=%p\n", list+i*chunkSize);
 
     endTime = MPI_Wtime();
-    printf("all used time:%f calc times:%ld cps:%lf\n", endTime-startTime, calcTimes, (double)calcTimes/(endTime-startTime));
-    saveList((orb*)list, nUnit, "./thelist1");
+    printf("all used time:%f calc times:%ld cps:%eg\n", endTime-startTime, calcTimes, (double)calcTimes/(endTime-startTime));
+    saveList((Orb*)list, nUnit, "./thelist1");
     free(list);
     list = NULL;
     MPI_Finalize();
     return 0;
 }
 
-void printList(float *list, int len, int rank, const char* s) {
+void printList(double *list, int len, int rank, const char* s) {
     //char str[1000] = {};
     int bufferlen = 100000;
     static char* str = NULL;
@@ -132,45 +133,45 @@ void printList(float *list, int len, int rank, const char* s) {
     }
     printf("%s, rank=%d list={\n%s}\n", s, rank, str);
 }
-void saveList(orb *olist, int len, const char* filepath) {
+void saveList(Orb *olist, int len, const char* filepath) {
 }
 /**/
-void loadList(float **list, int *len, const char* filepath) {
+void loadList(double **list, int *len, const char* filepath) {
 }
-void initList(orb *olist, int nUnit, int style) {
+void initList(Orb *olist, int nUnit, int style) {
     int i = 0;
     srandom(time(NULL));
     for (i=0; i<nUnit; ++i) {
-        //orb* o = (orb*)(list+i*unitSize);
-        orb* o = olist+i;
-        o->x  = (float)random()/(float)RAND_MAX*wide;
-        o->y  = (float)random()/(float)RAND_MAX*wide;
-        o->z  = (float)random()/(float)RAND_MAX*wide;
-        o->vx = (float)random()/(float)RAND_MAX*velo;
-        o->vy = (float)random()/(float)RAND_MAX*velo;
-        o->vz = (float)random()/(float)RAND_MAX*velo;
-        o->m  = (float)random()/(float)RAND_MAX*mass;
+        //Orb* o = (Orb*)(list+i*unitSize);
+        Orb* o = olist+i;
+        o->x  = (double)random()/(double)RAND_MAX*wide;
+        o->y  = (double)random()/(double)RAND_MAX*wide;
+        o->z  = (double)random()/(double)RAND_MAX*wide;
+        o->vx = (double)random()/(double)RAND_MAX*velo;
+        o->vy = (double)random()/(double)RAND_MAX*velo;
+        o->vz = (double)random()/(double)RAND_MAX*velo;
+        o->m  = (double)random()/(double)RAND_MAX*mass;
     }
 }
-void calcGravity(orb*o, orb*ta, float dist, float*gx, float*gy, float*gz) {
-    float a = ta->m / (dist*dist) * G;
+void calcGravity(Orb*o, Orb*ta, double dist, double*gx, double*gy, double*gz) {
+    double a = ta->m / (dist*dist) * G;
     *gx = - a * (o->x - ta->x) / dist;
     *gy = - a * (o->y - ta->y) / dist;
     *gz = - a * (o->z - ta->z) / dist;
 }
-/* orb update once with list */
-void calcOne(orb*o, int oId, orb*olist, int nUnit) {
-    //orb* mest = (orb*)*o;
+/* Orb update once with list */
+void calcOne(Orb*o, int oId, Orb*olist, int nUnit) {
+    //Orb* mest = (Orb*)*o;
     if (o->st == 0) {
         int i = 0;
-        float g, gx, gy, gz, gax, gay, gaz, dist = 0;
+        double g, gx, gy, gz, gax, gay, gaz, dist = 0;
         g = gx = gy = gz = gax = gay = gaz = 0;
         for (i=0; i<nUnit; ++i) {
-            orb* ta = (orb*)(olist+i);
+            Orb* ta = (Orb*)(olist+i);
             if (oId == i || ta->st == 1) {
                 continue;
             }
-            dist = sqrt((float)((ta->x-o->x)*(ta->x-o->x)+(ta->y-o->y)*(ta->y-o->y)+(ta->x-o->z)*(ta->z-o->z)));
+            dist = sqrt((double)((ta->x-o->x)*(ta->x-o->x)+(ta->y-o->y)*(ta->y-o->y)+(ta->x-o->z)*(ta->z-o->z)));
             int isTooRappid = dist*dist<(o->vx*o->vx+o->vy*o->vy+o->vz*o->vz)*10;
             if (dist<MIN_DIST || isTooRappid) {
                 // crash
